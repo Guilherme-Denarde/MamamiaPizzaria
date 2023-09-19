8668d60b7be2ab735440684131a868eb17dae41c
Kript0n007 <gabrielkuhnen38@gmail.com>
2023-09-19 13:30:37 -0300

    aaa

--- MammaMia/src/main/java/com/pizzeria/MammaMia/MammaMiaApplication.java
+++ MammaMia/src/main/java/com/pizzeria/MammaMia/MammaMiaApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MammaMiaApplication {
+public class
+MammaMiaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MammaMiaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MammaMiaApplication.class, args);
+    }
 
 }
