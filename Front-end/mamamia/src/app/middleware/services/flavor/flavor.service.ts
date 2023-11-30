f2709d898b45024c3269a2e63b262a9aa5200364
unknown <josesilveira4152@gmail.com>
2023-11-30 20:17:09 -0300

    shop

--- Front-end/mamamia/src/app/middleware/services/flavor/flavor.service.ts
+++ Front-end/mamamia/src/app/middleware/services/flavor/flavor.service.ts
@@ -11,7 +11,7 @@ import { CookieService } from 'ngx-cookie-service';
 export class FlavorService {
 
 
-  public readonly API: string = 'http://localhost:8080/sabor';
+  public readonly API: string = 'http://localhost:8081/sabor';
 
   constructor(private cookieService: CookieService,private http: HttpClient) { }
 
